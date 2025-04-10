180b104f6439238bba4c33f4543b74ec5d14a733
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-04-10 17:47:51 +0530

    Creating backend and making the app work along with imagekit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const ImageKit = require("imagekit");
+
+const port = process.env.PORT || 3000;
+
+const app = express()
+
+// SDK initialization
+
+
+const imageKit = new ImageKit({
+    publicKey : process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGE_KIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGE_KIT_END_POINT
+});
+
+app.get("/api/upload", (req, res) => {
+  const result = imageKit.getAuthenticationParameters();
+  res.send(result);
+})
+
+app.listen(port, () => {
+  console.log("Server running on port", port)
+})
