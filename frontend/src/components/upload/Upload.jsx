fade813472b2d9f53210b1909c63c9d1bde07ed6
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-04-23 16:08:36 +0530

    remove unnecessary files

--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -14,6 +14,7 @@ const authenticator = async () => {
     }
 
     const data = await response.json()
+    console.log(data, "data");
     const { signature, expire, token } = data;
     return { signature, expire, token }
   } catch (error) {
